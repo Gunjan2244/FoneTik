ac03d1fa44ed4e275896bc393a07ec5a4e9d92d8
Gunjan co-founder <akash2244everyone@gmail.com>
2025-11-25 19:48:49 +0530

    hii34

--- backend/main.py
+++ backend/main.py
@@ -5,11 +5,11 @@ from routes import auth, orders, complaints
 app = FastAPI()
 
 origins = [
-    "https://fone-tik.vercel.app/"
+    "https://fone-tik.vercel.app",
     "http://localhost:5173",
     "http://localhost:3000",
     "http://127.0.0.1:5173",
-    "http://127.0.0.1:3000",
+    "http://127.0.0.1",
     "http://localhost:5174",
     "http://127.0.0.1:5174",
     "http://127.0.0.1:34432",
